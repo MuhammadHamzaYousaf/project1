0b792cacf17f8693e30f9408d357822475bcdb88
Muhammad Hamza <chhamzay@gmail.com>
2020-10-18 23:47:31 +0500

    Project Updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const image1="https://picsum.photos/200/300";
+const image2="https://picsum.photos/201/300";
+const image3="https://picsum.photos/202/300";
+const image4="https://picsum.photos/203/300";
+const image5="https://picsum.photos/204/300";
+const image6="https://picsum.photos/208/300";
+const image7="https://picsum.photos/209/300";
+const image8="https://picsum.photos/210/300";
+const image9="https://picsum.photos/211/300";
+const image10="https://picsum.photos/212/300";
+const image11="https://picsum.photos/213/300";
+const image12="https://picsum.photos/214/300";
+const image13="https://picsum.photos/215/300";
+const image14="https://picsum.photos/216/300";
+const image15="https://picsum.photos/217/300";
+const image16="https://picsum.photos/218/300";
+const image17="https://picsum.photos/219/300";
+const image18="https://picsum.photos/220/300";
+const image19="https://picsum.photos/221/300";
+const image20="https://picsum.photos/222/300";
+const image21="https://picsum.photos/223/300";
+const image22="https://picsum.photos/224/300";
+const image23="https://picsum.photos/225/300";
+const image24="https://picsum.photos/226/300";
+const image25="https://picsum.photos/227/300";
+ReactDom.render(
+  <>
+  <h1 className="heading">Random Images Collections</h1>
+  
+  <div className="img_div">
+    <img src={image20} alt="Random Images"/>
+  </div>
+  <div className="img_div">
+    <img src={image1} alt="Random Images"/>
+    <img src= {image2} alt="Random Images"/>
+    <img src={image3} alt="Random Images"/>
+  </div>
+  <div className="img_div">
+    <img src= {image4} alt="Random Images"/>
+    <img src={image5} alt="Random Images"/>
+    <img src= {image6} alt="Random Images"/>
+    <img src= {image7} alt="Random Images"/>
+    <img src={image8} alt="Random Images"/>
+  </div>
+  <div className="img_div">
+    <img src= {image9} alt="Random Images"/>
+    <img src={image10} alt="Random Images"/>
+    <img src= {image11} alt="Random Images"/>
+    <img src= {image12} alt="Random Images"/>
+    <img src={image13} alt="Random Images"/>
+    <img src= {image14} alt="Random Images"/>
+    <img src={image15} alt="Random Images"/>
+  </div>
+  <div className="img_div">
+    <img src= {image16} alt="Random Images"/>
+    <img src={image17} alt="Random Images"/>
+    <img src= {image18} alt="Random Images"/>
+    <img src= {image19} alt="Random Images"/>
+    <img src={image20} alt="Random Images"/>
+    <img src= {image14} alt="Random Images"/>
+    <img src={image15} alt="Random Images"/>
+    <img src= {image6} alt="Random Images"/>
+    <img src={image8} alt="Random Images"/>
+  </div>  
+  </>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/index1.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <h1>This is Heading 1</h1>,
+    document.getElementById('root'));
